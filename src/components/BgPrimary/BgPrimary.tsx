ec04e0c6f2220534c1eeb861677d97de83205c70
eram-iw <eram.perween@iwebcode.dev>
2022-07-06 10:21:46 +0530

    added react counter

--- src/components/BgPrimary/BgPrimary.tsx
+++ src/components/BgPrimary/BgPrimary.tsx
@@ -1,30 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import CountUp from 'react-countup';
 
 function BgPrimary() {
-    const [num, setNum] = useState({
-        loc: 0,
-        nop: 0,
-        noc: 0
-    })
-    const [loc, setLoc] = useState(0)
-    useEffect(() => {
-        lineOfCodes()
-    }, [loc])
-
-
-    const lineOfCodes = () => {
-        setInterval(function () {
-            if (loc < 99399) {
-                setLoc(loc + 7647)
-            }
-            else{
-                setLoc(99399)
-            }
-            console.log(loc);
-        }, 100);
-        
-    }
-
+   
 
     return (
         <div className="templateux-section bg-primary" id="templateux-counter-section">
@@ -32,19 +10,21 @@ function BgPrimary() {
                 <div className="row">
                     <div className="col-md">
                         <div className="templateux-counter text-center">
-                            <span className="templateux-number" data-number="99399">{loc}</span>
+                            <span className="templateux-number" data-number="99399">
+                                <CountUp end={99399} />
+                            </span>
                             <span className="templateux-label">Lines of Codes</span>
                         </div>
                     </div>
                     <div className="col-md">
                         <div className="templateux-counter text-center">
-                            <span className="templateux-number" data-number="99">{num.nop}</span>
+                            <span className="templateux-number" data-number="99"><CountUp end={99}/></span>
                             <span className="templateux-label">Number of Projects</span>
                         </div>
                     </div>
                     <div className="col-md">
                         <div className="templateux-counter text-center">
-                            <span className="templateux-number" data-number="124">{num.noc}</span>
+                            <span className="templateux-number" data-number="124"><CountUp end={124} /></span>
                             <span className="templateux-label">Number of Clients</span>
                         </div>
                     </div>
