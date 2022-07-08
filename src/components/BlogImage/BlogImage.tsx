639cb195f43f45c2a1304e26c58111817bb156ad
eram-iw <eram.perween@iwebcode.dev>
2022-07-08 11:34:53 +0530

    working on blog's slider

--- src/components/BlogImage/BlogImage.tsx
+++ src/components/BlogImage/BlogImage.tsx
@@ -2,119 +2,41 @@ import React from 'react'
 import './BlogImage.css'
 import { FaAngleLeft } from "react-icons/fa";
 import { FaAngleRight } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
+import SingleSlide from './SingleSlide';
 
 const BlogImage = () => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    };
     return (
         <div className="templateux-section">
             <div className="container">
-                <div className="row">
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-2.jpg.pagespeed.ic.r9N2neKBEW.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-3.jpg.pagespeed.ic.vCxQ_fqCHp.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-1.jpg.pagespeed.ic.abj2HoBrGU.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_2.jpg.pagespeed.ic.m_Abwozv89.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_3.jpg.pagespeed.ic.F3kN-euvoT.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/ximg_4.jpg.pagespeed.ic.r3Uo0w6spo.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-2.jpg.pagespeed.ic.r9N2neKBEW.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-3.jpg.pagespeed.ic.vCxQ_fqCHp.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-1.jpg.pagespeed.ic.abj2HoBrGU.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_2.jpg.pagespeed.ic.m_Abwozv89.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_3.jpg.pagespeed.ic.F3kN-euvoT.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-                    <div className="col-md-6 col-lg-4 mb-4">
-                        <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/ximg_4.jpg.pagespeed.ic.r3Uo0w6spo.webp)' }} data-aos="fade" data-aos-delay="300">
-                            <div className="block-thumbnail-content">
-                                <h2>Behind the word mountains</h2>
-                                <span className="post-meta">January 20, 2019 • 3 Comments</span>
-                            </div>
-                        </a>
-                    </div>
-
-                </div>
-                <div className="row mt-5">
-                    <div className="col-md-12 pt-5 row custom" >
-                        <button className='col prev'> <FaAngleLeft /></button>
-                        <button className='col mx-2 active'>1</button>
-                        <button className='col mx-2'>2</button>
-                        <button className='col mx-2'>3</button>
-                        <button className='col next'><FaAngleRight /> </button>
-                    </div>
-                </div>
+                <Swiper
+                    cssMode={true}
+                    loop={true}
+                    navigation={true}
+                    pagination={pagination}
+                    mousewheel={true}
+                    keyboard={true}
+                    modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <SingleSlide/>
+                    </SwiperSlide>
+                    <SwiperSlide> <SingleSlide /></SwiperSlide>
+                    <SwiperSlide> <SingleSlide /></SwiperSlide>
+                </Swiper>
             </div>
         </div >
     )
---
+++ src/components/BlogImage/SingleSlide.tsx
@@ -0,0 +1,109 @@
+import React from 'react'
+
+const SingleSlide = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-2.jpg.pagespeed.ic.r9N2neKBEW.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-3.jpg.pagespeed.ic.vCxQ_fqCHp.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-1.jpg.pagespeed.ic.abj2HoBrGU.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_2.jpg.pagespeed.ic.m_Abwozv89.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_3.jpg.pagespeed.ic.F3kN-euvoT.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/ximg_4.jpg.pagespeed.ic.r3Uo0w6spo.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-2.jpg.pagespeed.ic.r9N2neKBEW.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-3.jpg.pagespeed.ic.vCxQ_fqCHp.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xslider-1.jpg.pagespeed.ic.abj2HoBrGU.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_2.jpg.pagespeed.ic.m_Abwozv89.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/xhero_3.jpg.pagespeed.ic.F3kN-euvoT.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+                <div className="col-md-6 col-lg-4 mb-4">
+                    <a href="blog-single.html" className="block-thumbnail-1 one-whole show-text height-sm aos-init aos-animate" style={{ backgroundImage: 'url(https://preview.colorlib.com/theme/atomic/images/ximg_4.jpg.pagespeed.ic.r3Uo0w6spo.webp)' }} data-aos="fade" data-aos-delay="300">
+                        <div className="block-thumbnail-content">
+                            <h2>Behind the word mountains</h2>
+                            <span className="post-meta">January 20, 2019 • 3 Comments</span>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default SingleSlide
